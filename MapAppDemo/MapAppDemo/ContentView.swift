460f882c00d6ad1698a40bb953bf67b7a95740aa
Sherwin Espela <sherwin.espela@gmail.com>
2020-03-31 20:17:08 -0700

    [develop] created xcode project
    * created xcode project for the Map app demo
    * added MapView component

---
+++ MapAppDemo/MapAppDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MapAppDemo
+//
+//  Created by Sherwin Espela on 2020-03-31.
+//  Copyright © 2020 Sherwin Espela. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MapView()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
